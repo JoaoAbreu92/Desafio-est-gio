ae1a380e81a3db10d9b1e137c9d8cd49641cd6be
JoaoAbreu92 <heriquepib@gmail.com>
2023-02-10 19:34:00 -0300

    inicio da funcao cadastro

---
+++ cadastro.js
@@ -0,0 +1,9 @@
+/* Inicio da função cadastro */
+function cadastroUser(){
+var nome = document.getElementById("nome");
+var email = document.getElementById("email");
+var celular = document.getElementById("celular");
+var fixo = document.getElementById("fixo");
+
+}
+
